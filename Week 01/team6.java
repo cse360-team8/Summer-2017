d6eafdee85a62addb74e45d62a612fe28e5df9f9
mbwarnic <michael-warnick@live.com>
2017-05-23 12:15:09 -0700

    Add files via upload

---
+++ Week 01/team6.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.event.*;
+
+public class team6 extends JPanel{
+
+
+	public team6()
+   {
+
+	//setLayout(new BorderLayout());
+	setBackground(Color.RED);
+
+
+
+     JButton joel = new JButton("Joel Menja");
+	 JButton Manny = new JButton("Manuel Ucles");
+     JButton sam = new JButton("Michael warnick");
+
+     add(joel);
+     add(Manny);
+     add(sam);
+
+    }
+
+}
